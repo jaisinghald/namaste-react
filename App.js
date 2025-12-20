4caa25592fe0b61ced543b696877fb8a3c1f31d2
jaisinghald <jaiparallel@gmail.com>
2025-12-21 00:19:36 +0530

    Episode -01

---
+++ App.js
@@ -0,0 +1,22 @@
+
+//create nested tag
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" },
+        [React.createElement("h1", {}, "i am h1 tag"),
+        React.createElement("h2", {}, "i am h2 tag")]
+    )
+    , React.createElement("div", { id: "child2" },
+        [React.createElement("h1", {}, "i am h1 tag"),
+        React.createElement("h2", {}, "i am h2 tag")]
+    )])
+
+
+const heading = React.createElement("h1",
+    {
+        id: "heading",
+        xyz: "abc"
+    },
+    'Namaste React')
+console.log(parent)//object
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)

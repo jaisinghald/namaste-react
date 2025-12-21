af78ed81db89faaf094afb301becd6886ba24042
jaisinghald <jaiparallel@gmail.com>
2025-12-21 14:30:23 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom/client"; // for React 18+
 
 //create nested tag
 const parent = React.createElement("div", { id: "parent" }, [
@@ -10,13 +12,7 @@ const parent = React.createElement("div", { id: "parent" }, [
         React.createElement("h2", {}, "i am h2 tag")]
     )])
 
-
-const heading = React.createElement("h1",
-    {
-        id: "heading",
-        xyz: "abc"
-    },
-    'Namaste React')
+ 
 console.log(parent)//object
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(parent)
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 #React application 
+# Parcel
+  --dev build 
+  --local server
+  --HMR= Hot module replacement
+  --File watcher algo use to refresh data when user change anything and save
+  --caching-faster build
+  --image optimization
+  --Minification
+  --Bundling
+  --Compressing file
+  --Consistent hashing
+  --code spiliting 
+  --differencial bundling -to suport older version browser
+  --Error handling
+  --dignostice
+  --https
+  --Tree shaking -remove unnessary file
